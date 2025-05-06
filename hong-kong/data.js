f73a3194e33597e6a55a35c7f22ec45445335ef6
bartendly <martial@bartendly.com>
2025-05-06 15:49:43 +0200

    Add files via upload

--- hong-kong/data.js
+++ hong-kong/data.js
@@ -1,4 +1,6 @@
-window.bartenders = [
-    { name: "Amira Khalid", bar: "The Dunes Bar", img: "amira-khalid", alt: "Amira Khalid crafting a signature cocktail" },
-    { name: "Omar Farooq", bar: "Palm Sky Lounge", img: "omar-farooq", alt: "Omar Farooq preparing a drink under the Dubai skyline" }
+  window.bartenders = [
+    { name: "Alex Lee", bar: "Dragon Lounge", img: "alex-lee", alt: "Alex Lee mixing a cocktail at Dragon Lounge"},
+    { name: "Mei Chan", bar: "The Lotus Bar", img: "mei-chan", alt: "Mei Chan smiling behind the bar at The Lotus Bar"},
+    { name: "Rafael Santos", bar: "Jade & Juniper", img: "rafael-santos", alt: "Rafael Santos shaking a drink at Jade & Juniper"},
+    { name: "Yuki Nakamura", bar: "Velvet Pearl", img: "yuki-nakamura", alt: "Yuki Nakamura preparing a cocktail at Velvet Pearl"}
   ];
