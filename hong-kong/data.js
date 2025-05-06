4f9d8d981618f173102d45c3e3a660768334f9ab
bartendly <martial@bartendly.com>
2025-05-06 15:45:35 +0200

    Add files via upload

---
+++ hong-kong/data.js
@@ -0,0 +1,4 @@
+window.bartenders = [
+    { name: "Amira Khalid", bar: "The Dunes Bar", img: "amira-khalid", alt: "Amira Khalid crafting a signature cocktail" },
+    { name: "Omar Farooq", bar: "Palm Sky Lounge", img: "omar-farooq", alt: "Omar Farooq preparing a drink under the Dubai skyline" }
+  ];
