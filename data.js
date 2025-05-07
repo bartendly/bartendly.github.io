c87e309646f7b40cf0c0361685e5905a39a85e6c
bartendly <martial@bartendly.com>
2025-05-07 20:24:18 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+  window.bartenders = [
+    { name: "Alex Lee", bar: "Dragon Lounge", img: "alex-lee", alt: "Alex Lee mixing a cocktail at Dragon Lounge"},
+    { name: "Mei Chan", bar: "The Lotus Bar", img: "mei-chan", alt: "Mei Chan smiling behind the bar at The Lotus Bar"},
+    { name: "Rafael Santos", bar: "Jade & Juniper", img: "rafael-santos", alt: "Rafael Santos shaking a drink at Jade & Juniper"},
+    { name: "Yuki Nakamura", bar: "Velvet Pearl", img: "yuki-nakamura", alt: "Yuki Nakamura preparing a cocktail at Velvet Pearl"}
+  ];
