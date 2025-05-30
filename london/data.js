904be11650cfca3b37777395fca66610f3ecf102
bartendly <martial@bartendly.com>
2025-05-30 11:25:01 +0200

    Add files via upload

--- london/data.js
+++ london/data.js
@@ -1,5 +1,5 @@
  window.bartenders = [
-    { name: "Giuseppe Quartararo", bar: "Nightjar", img: "london/images/giuseppe-quartararo", alt: "Giuseppe Quartararo serving a cocktail at The Nightjar in Shoreditch"},
+    { name: "Giuseppe Quartararo", bar: "Nightjar", img: "giuseppe-quartararo", alt: "Giuseppe Quartararo serving a cocktail at The Nightjar in Shoreditch"},
     { name: "Jayden Brooks", bar: "Camden Spirits", img: "jayden-brooks", alt: "Jayden Brooks pouring a smoked cocktail at Camden Spirits"},
     { name: "Sophie Allen", bar: "The Botanist", img: "sophie-allen", alt: "Sophie Allen preparing a gin-based drink at The Botanist in Chelsea"},
     { name: "Daniel Wright", bar: "Skylight Lounge", img: "daniel-wright", alt: "Daniel Wright mixing drinks at the rooftop Skylight Lounge"},
