17ec78dbf561f1021fe84aa30536c2a8c38f78ea
bartendly <martial@bartendly.com>
2025-05-30 11:06:48 +0200

    Add files via upload

--- london/data.js
+++ london/data.js
@@ -1,8 +1,7 @@
  window.bartenders = [
-    { name: "Ella Thompson", bar: "The Velvet Fox", img: "ella-thompson", alt: "Ella Thompson serving a cocktail at The Velvet Fox in Soho"},
+    { name: "Giuseppe Quartararo", bar: "Nightjar", img: "giuseppe-quartararo", alt: "Giuseppe Quartararo serving a cocktail at The Nightjar in Shoreditch"},
     { name: "Jayden Brooks", bar: "Camden Spirits", img: "jayden-brooks", alt: "Jayden Brooks pouring a smoked cocktail at Camden Spirits"},
     { name: "Sophie Allen", bar: "The Botanist", img: "sophie-allen", alt: "Sophie Allen preparing a gin-based drink at The Botanist in Chelsea"},
     { name: "Daniel Wright", bar: "Skylight Lounge", img: "daniel-wright", alt: "Daniel Wright mixing drinks at the rooftop Skylight Lounge"},
     { name: "Aisha Malik", bar: "The Alchemy Room", img: "aisha-malik", alt: "Aisha Malik creating a flaming cocktail at The Alchemy Room in Shoreditch"}
   ];
-  
